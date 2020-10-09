e1239f83ee93c6f791cd3d8dd56ea931c9d14390
Alonso Garza <JuanAGC@soriana.com>
2020-10-09 01:55:29 -0500

    VUE: WIP: Se avanza ::ISSUE:: No Aplica

--- docs/curso/js/03.js
+++ docs/curso/js/03.js
@@ -2,11 +2,26 @@ const app = new Vue({
     el: '#app',
     data:{
         titulo: 'GYM con VUE',
+        tareas: [],
+        nuevaTarea: '',
         //-- Mis Notas de lo que se aplica --
         aplica: [
-            {tip: 'interpolacion {{titulo}}'},
+            {tip: 'interpolacion {{titulo}} // de JS a HTML'},
+            {tip: 'v-model="nuevaTarea" // de HTML a JS'},
+            {tip: '@click="agregarTarea" // click al mouse'},
+            {tip: '@keyup.enter="agregarTarea" // enter desde teclado'},            
         ]
 
+    },
+    methods:{
+        agregarTarea: function(){
+            // console.log(`Distinte clik ${this.nuevaTarea}`)
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            this.nuevaTarea = "";
+        }
     }
 
 })

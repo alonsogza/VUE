b417b1dec2b21570ec33f4b256ef8084fe18ec81
Alonso Garza <JuanAGC@soriana.com>
2020-10-17 01:32:03 -0500

    WIP: Incluyendo VUEX

---
+++ docs/curso/js/09vuex.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        titulo: 'Comunicacion entre Componentes ( VUEX )',
+        mensaje: 'Componente',
+        //-- Mis Notas de lo que se aplica --
+        aplica: [
+            {tip: 'interpolacion funcionan igual: v-text="titulo" ó {{titulo}}'},
+            {tip: 'interpolacion {{titulo}} // de JS a HTML'},
+            {tip: 'interpolacion v-text="titulo" // de JS a HTML'},
+            {tip: 'Componente: // Componente en JS'},            
+            {tip: 'Templeate: // Componente en JS'},                        
+            {tip: 'Props: // Componente en JS'},                        
+        ]
+    }        
+
+})
+
+/* 
+
+IMPORTANTE 02: dentro del template se puede ver el codgio HTML de color se requiere instalar
+la extension --> es6-string-html
+
+*/

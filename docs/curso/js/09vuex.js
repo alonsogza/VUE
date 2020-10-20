3c86540fa5a1dce9f12180ead18cce53eba592e4
Alonso Garza <alonso_03@hotmail.com>
2020-10-20 02:00:49 -0500

    VUE: Se concluye el ejercicio de la libreria VUEX ::ISSUE:: No Aplica

--- docs/curso/js/09vuex.js
+++ docs/curso/js/09vuex.js
@@ -1,5 +1,55 @@
+// Link de VUEX: https://vuex.vuejs.org/#what-is-a-state-management-pattern
+
+// Creamos el COMPONENTE Padre
+// Se puede crear un nuevo componente, en este caso es HIJO
+Vue.component('titulo',{
+    template: /*html*/
+        `
+        <div>
+            <h1 class='text-primary font-weight-light'>numero {{$store.state.numero}}</h1>
+            <p class="text-muted">
+                Companente Hijo (boton)
+            </p>            
+            <hijo></hijo>
+        </div>        
+        `
+})
+
+// Creamos el COMPONENTE HIJO
+Vue.component('hijo', {
+    template: /*html */
+        `
+        <div>
+            <button type="button" class="btn btn-secondary" @click="$store.commit('aumentar')">+</button>
+            <!-- Comprobacion que la informacion de NUMERO puede ser leida con el hijo -->
+            <h1 class='text-secondary font-weight-light'>numero {{$store.state.numero}}</h1>            
+        </div>        
+        `
+})
+
+// Esta constante la ocupa VUEX para empezar a realizar la comunicacion entr componentes
+const store = new Vuex.Store({
+    state: {
+        numero: 10
+    },
+    mutations:{
+        aumentar(state){
+            state.numero ++
+
+        }
+    }
+})
+
+
+// Instancia Normal de VUE
 const app = new Vue({
     el: "#app",
+    store: store, // Es para que funcione VUEX
+/* Para JS es lo mismo así:
+    --> store: store
+    y asi 
+    --> store
+*/        
     data: {
         titulo: 'Comunicacion entre Componentes ( VUEX )',
         mensaje: 'Componente',
@@ -10,7 +60,7 @@ const app = new Vue({
             {tip: 'interpolacion v-text="titulo" // de JS a HTML'},
             {tip: 'Componente: // Componente en JS'},            
             {tip: 'Templeate: // Componente en JS'},                        
-            {tip: 'Props: // Componente en JS'},                        
+            {tip: 'Vuex.Store, state, commit  : // Establecer Comunnicacion entre Componente en JS'},                        
         ]
     }        
 
